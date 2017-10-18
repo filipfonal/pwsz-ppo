845a38f5f4d3702cb5140dfbb35d476278021801
Filip <filipfonal@gmail.com>
2017-10-18 16:44:47 +0200

    Lab03

--- lab03/lab03.cpp
+++ lab03/lab03.cpp
@@ -8,18 +8,14 @@ class Point {
 	public:
 		int a;
 		int b;
-		int c;
-		int d;
 		
 		Point() {
 			//cout << "Point has been created." << endl;
 		}
 		
-		Point(int a, int b, int c, int d) {
+		Point(int a, int b) {
 			this->a = a;
 			this->b = b;
-			this->c = c;
-			this->d = d;
 			//cout << "Point [" << this->x << ", " << this->y << "] has been created." << endl;
 		}
 		
@@ -29,43 +25,43 @@ class Point {
 		
 };
 
-class Circle {
-	public:
-		Point center;
-		int radius;
-		
-		Circle(Point center, int radius) {
-			this->center = center;
-			this->radius = radius;
-		}
-		
-		void getCoordinates() {
-			cout << "x: " << this->center.x << endl << "y: " << this->center.y << endl;
-		}
-};
 
 class Square{
 	public:
-		Point p;
-		int d;
+		Point a;
+		Point b;
+		Point c;
+		Point d;
+		int l;
 		
-		Square(Point p, int d){
-			this->p = p;
-			this->d = d;
+		Square(Point a, int l){
+			this->a = a;
+			this->l = l;
+		}
+		
+		void getSquare() {
+			cout << "x1: " << this->a.a  << " " << "y1: " << this->a.b << endl;
+			cout << "x2: " << this->a.a+l << " " << "y2: " << this->a.b << endl;
+			cout << "x3: " << this->a.a << " " << "y3: " << this->a.b+l<< endl;
+			cout << "x4: " << this->a.a+l << " " << "y4: " << this->a.b+l<< endl;
 		}
 };
 
 int main() {
 	srand(time(NULL));
 	
-	int inputA = 0, inputB = 0, inputC = 0, inputD = 0;
-	int inputRadius = 5;
+	int inputA = 0, inputB = 0;
+	int inputLength = 5;
 	
-	Point p = Point(inputA, inputB, inputC, inputD);
-	Circle c = Circle(p, inputRadius);
+	cout << "x1: ";
+	 cin >> inputA;
+	cout << "y1: ";
+	 cin >> inputB;
 	
-	c.center.movePoint(rand() % 10, rand() % 10);
-	c.getCoordinates();
+	Point p = Point(inputA, inputB);
+	Square s =Square(p, inputLength);
+	
+	s.getSquare();
 	
 	return 0;
 }
